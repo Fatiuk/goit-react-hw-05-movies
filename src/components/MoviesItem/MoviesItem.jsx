86070ec2f163554de273429aa9bb01426cc718cf
Fatiuk <125760945+Fatiuk@users.noreply.github.com>
2023-09-30 02:37:56 +0300

    Update 0.0.5

--- src/components/MoviesItem/MoviesItem.jsx
+++ src/components/MoviesItem/MoviesItem.jsx
@@ -1,7 +1,22 @@
 import { useLocation } from 'react-router-dom';
-import { MovieItem, MovieLink } from './MoviesItem.styled';
+import {
+  MovieItem,
+  MovieLink,
+  MovieInfo,
+  AdultInfo,
+} from './MoviesItem.styled';
 
-const MoviesItem = ({ movie: { id, title, poster_path } }) => {
+const MoviesItem = ({
+  movie: {
+    id,
+    title,
+    poster_path,
+    adult,
+    popularity,
+    vote_average,
+    vote_count,
+  },
+}) => {
   const imageUrl = poster_path
     ? `https://image.tmdb.org/t/p/w200/${poster_path}`
     : 'https://via.placeholder.com/200x300';
@@ -11,6 +26,14 @@ const MoviesItem = ({ movie: { id, title, poster_path } }) => {
         <img src={imageUrl} alt={title} loading="lazy" />
         <p>{title}</p>
       </MovieLink>
+      <MovieInfo>
+        <ul>
+          <li>Popularity: {popularity.toFixed(0)}</li>
+          <li>Rating: {vote_average.toFixed(1)}</li>
+          <li>Votes: {vote_count}</li>
+        </ul>
+      </MovieInfo>
+      {adult && <AdultInfo>18+</AdultInfo>}
     </MovieItem>
   );
 };
--- src/components/MoviesItem/MoviesItem.styled.jsx
+++ src/components/MoviesItem/MoviesItem.styled.jsx
@@ -2,14 +2,75 @@ import styled from 'styled-components';
 import { Link } from 'react-router-dom';
 
 export const MovieItem = styled.li`
+  position: relative;
   max-width: 300px;
+  overflow: hidden;
+  border-radius: 8px;
+  transition: all 0.3s ease;
+  &:hover {
+    cursor: pointer;
+    color: #fff;
+    background-color: rgba(0, 77, 64, 0.8);
+    box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
+  }
+`;
+
+export const MovieInfo = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  position: absolute;
+  top: 0;
+  left: -100%;
+  width: 100%;
+  height: 300px;
+  padding: 10px;
+  text-align: start;
+  font-size: 18px;
+  color: #fff;
+  background-color: rgba(0, 0, 0, 0.5);
+  transition: all 0.3s ease;
+  pointer-events: none;
+  ul {
+    list-style: none;
+  }
+  li {
+    margin-bottom: 20px;
+  }
 `;
 
 export const MovieLink = styled(Link)`
   text-decoration: none;
-  color: #191d1e;
+  color: inherit;
+  transition: all 0.3s ease;
+
+  p {
+    margin: 0;
+    padding: 10px;
+    max-width: 200px;
+    text-align: center;
+    font-size: 16px;
+  }
 
   &:hover {
-    color: #004d40;
+    cursor: pointer;
+    color: #fff;
+    + ${MovieInfo} {
+      left: 0;
+    }
   }
+`;
+
+export const AdultInfo = styled.div`
+  position: absolute;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  top: 10px;
+  right: 10px;
+  width: 36px;
+  height: 36px;
+  font-weight: 700;
+  border-radius: 50%;
+  background-color: rgb(255, 255, 255);
 `;
